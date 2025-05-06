868c95dd5907d9c55028f52ce6b9b7e3b7ccb322
Felipe Martins Buccini <167038380+Iniccub@users.noreply.github.com>
2025-05-06 16:59:27 -0300

    Add files via upload

---
+++ perguntas_por_fornecedor.py
@@ -0,0 +1,75 @@
+perguntas_por_fornecedor = {
+    'CANTINA FREITAS': {
+        'Documentação': [
+            '1 - Os documentos obrigatórios para análise e faturamento foram entregues dentro do prazo acordado em contrato?',
+            '2 - A contratada apresentou todas as documentações exigidas, conforme contrato com os devidos recolhimentos e pagamentos?',
+            '3 - A Nota Fiscal foi emitida com dados corretos?',
+            '4 - CND-FGTS e CRT, relativos à Regularidade Fiscal e Trabalhista, estão atualizados?',
+            '5 - Apresentam à SIC cópia autenticada do ALVARÁ DE FUNCIONAMENTO, expedido pelos órgãos competentes, por meio do qual a CONTRATADA ficará autorizada a realizar suas atividades comerciais',
+        ],
+    },
+    'EXPRESSA TURISMO LTDA': {
+        'Documentação': [
+            '1 - Os documentos obrigatórios para análise e faturamento foram entregues dentro do prazo acordado em contrato?',
+            '2 - A contratada apresentou todas as documentações exigidas, conforme contrato com os devidos recolhimentos e pagamentos?',
+            '3 - A Nota Fiscal foi emitida com dados corretos?',
+            '4 - A contratada possui autorizações atualizadas (licenças específicas) que a habilitem para a prestação de serviços de transporte, expedidas pelos órgãos competentes (ANTT, DER, DETRAN, BHTRANS e/ou quaisquer outros);5 - A Contratada possui seguro de Acidentes pessoais, extensivo aos passageiros, e outros exigidos por Lei?'
+        ],
+    },
+    'LEAL VIAGENS E TURISMO': {
+        'Documentação': [
+            '1 - Os documentos obrigatórios para análise e faturamento foram entregues dentro do prazo acordado em contrato?',
+            '2 - A contratada apresentou todas as documentações exigidas, conforme contrato com os devidos recolhimentos e pagamentos?',
+            '3 - A Nota Fiscal foi emitida com dados corretos?',
+            '4 - A contratada possui autorizações atualizadas (licenças específicas) que a habilitem para a prestação de serviços de transporte, expedidas pelos órgãos competentes (ANTT, DER, DETRAN, BHTRANS e/ou quaisquer outros);5 - A Contratada possui seguro de Acidentes pessoais, extensivo aos passageiros, e outros exigidos por Lei?',
+        ],
+    },
+    'ACREDITE EXCURSÕES E EXPOSIÇÕES INTINERANTE LTDA': {
+        'Documentação': [
+            '1 - Os documentos obrigatórios para análise e faturamento foram entregues dentro do prazo acordado em contrato?',
+            '2 - A contratada apresentou todas as documentações exigidas, conforme contrato com os devidos recolhimentos e pagamentos?',
+            '3 - A Nota Fiscal foi emitida com dados corretos?',
+            '4 - A contratada possui autorizações atualizadas (licenças específicas) que a habilitem para a prestação de serviços de transporte, expedidas pelos órgãos competentes (ANTT, DER, DETRAN, BHTRANS e/ou quaisquer outros);5 - A Contratada possui seguro de Acidentes pessoais, extensivo aos passageiros, e outros exigidos por Lei?',
+        ],
+    },
+    'REAL VANS LOCAÇÕES': {
+        'Documentação': [
+            '1 - Os documentos obrigatórios para análise e faturamento foram entregues dentro do prazo acordado em contrato?',
+            '2 - A contratada apresentou todas as documentações exigidas, conforme contrato com os devidos recolhimentos e pagamentos?',
+            '3 - A Nota Fiscal foi emitida com dados corretos?',
+            '4 - A contratada possui autorizações atualizadas (licenças específicas) que a habilitem para a prestação de serviços de transporte, expedidas pelos órgãos competentes (ANTT, DER, DETRAN, BHTRANS e/ou quaisquer outros);5 - A Contratada possui seguro de Acidentes pessoais, extensivo aos passageiros, e outros exigidos por Lei?',
+        ],
+    },
+    'AC TRANSPORTES E SERVIÇOS LTDA - ACTUR': {
+        'Documentação': [
+            '1 - Os documentos obrigatórios para análise e faturamento foram entregues dentro do prazo acordado em contrato?',
+            '2 - A contratada apresentou todas as documentações exigidas, conforme contrato com os devidos recolhimentos e pagamentos?',
+            '3 - A Nota Fiscal foi emitida com dados corretos?',
+            '4 - A contratada possui autorizações atualizadas (licenças específicas) que a habilitem para a prestação de serviços de transporte, expedidas pelos órgãos competentes (ANTT, DER, DETRAN, BHTRANS e/ou quaisquer outros);5 - A Contratada possui seguro de Acidentes pessoais, extensivo aos passageiros, e outros exigidos por Lei?',
+        ],
+    },
+    'TRANSCELO TRANSPORTES LTDA': {
+        'Documentação': [
+            '1 - Os documentos obrigatórios para análise e faturamento foram entregues dentro do prazo acordado em contrato?',
+            '2 - A contratada apresentou todas as documentações exigidas, conforme contrato com os devidos recolhimentos e pagamentos?',
+            '3 - A Nota Fiscal foi emitida com dados corretos?',
+            '4 - A contratada possui autorizações atualizadas (licenças específicas) que a habilitem para a prestação de serviços de transporte, expedidas pelos órgãos competentes (ANTT, DER, DETRAN, BHTRANS e/ou quaisquer outros);5 - A Contratada possui seguro de Acidentes pessoais, extensivo aos passageiros, e outros exigidos por Lei?',
+        ],
+    },
+    'MINASCOPY NACIONAL EIRELI': {
+        'Documentação': [
+            '1 - Os documentos obrigatórios para análise e faturamento foram entregues dentro do prazo acordado em contrato?',
+            '2 - A contratada apresentou todas as documentações exigidas, conforme contrato com os devidos recolhimentos e pagamentos?',
+            '3 - A Nota Fiscal foi emitida com dados corretos?',
+            '4 - CND e CRT, relativos à Regularidade Fiscal e Trabalhista, estão atualizados?',
+        ],
+    },
+    'OTIMIZA VIGILÂNCIA E SEG. PATRIMONIAL': {
+        'Documentação': [
+            '1 - Os documentos obrigatórios para análise e faturamento foram entregues dentro do prazo acordado em contrato?',
+            '2 - A contratada apresentou todas as documentações exigidas, conforme contrato com os devidos recolhimentos e pagamentos?',
+            '3 - A Nota Fiscal foi emitida com dados corretos?',
+            '4 - CND e CRT, relativos à Regularidade Fiscal e Trabalhista, estão atualizados?',
+        ],
+    },
+}
